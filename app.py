b46e377dc5da957890defe3be56dc752e6fd6241
susana474 <159489427+susana474@users.noreply.github.com>
2025-02-25 15:21:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ async def get_answer(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
